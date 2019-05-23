9e7848bda87e4bb869d861f1fe60a74a7902a91d
Jeff Manke <jeffman879@gmail.com>
2019-05-22 20:19:38 -0700

    Unity project

---
+++ PIVR/UnityPIVR/Assets/Scripts/ModelDataGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelDataGenerator : MonoBehaviour
+{
+    public Camera refCam;
+    public Transform target;
+    public string outputPath;
+    public float minDist = 0.1f;
+    public float maxDist = 2f;
+    public float increment = 0.01f;
+
+    private Plane[] planes;
+    private Collider objCollider;
+
+    private void Start()
+    {
+        planes = GeometryUtility.CalculateFrustumPlanes(refCam);
+        objCollider = target.GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        if (GeometryUtility.TestPlanesAABB(planes, objCollider.bounds))
+        {
+            Debug.Log(target.name + " has been detected!");
+        }
+        else
+        {
+            Debug.Log("Nothing has been detected");
+        }
+    }
+}
---
+++ PIVR/UnityPIVR/Assets/Scripts/Webcam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+    public RawImage rawimage;
+    public int deviceIndex = 0;
+
+    void Start()
+    {
+        var devices = WebCamTexture.devices;
+
+        foreach (var device in devices)
+        {
+            Debug.Log(device.name);
+        }
+
+        var webcamTexture = new WebCamTexture(devices[deviceIndex].name);
+        rawimage.texture = webcamTexture;
+
+        Debug.Log("Resolution = " + rawimage.texture.width + ", " + rawimage.texture.height);
+
+        rawimage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
